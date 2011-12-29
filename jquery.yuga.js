11d3c0c11e8b88ce1a60c395936324f4ed8f717a
Kyosuke Nakamura <nakamura@pxgrid.com>
2011-12-29 21:12:17 +0900

    jslintでの指摘を修正。

--- jquery.yuga.js
+++ jquery.yuga.js
@@ -28,13 +28,15 @@
 		var conf = $.extend({
 			suffix: '_on',
 			group: 'a'
-		}, options)
+		}, options);
 		return this.each(function(){
 			var img = $(this);
 			var a = img.parents(conf.group);
 			var target = (a.length) ? a : img;
 			var src = img.attr('src');
-			if (!src) return;
+			if (!src) {
+				return;
+			}
 			var src_o = src.replace(/\.\w+$/, conf.suffix + '$&');
 
 			$.yuga.preloadImg(src_o);
